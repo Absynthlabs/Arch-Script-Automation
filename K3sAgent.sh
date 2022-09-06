e700989fbf4ba9290158483cb2ab7d9d69283525
zkryr <108679865+zkryr@users.noreply.github.com>
2022-09-06 14:25:54 +0300

    Update K3sAgent.sh

--- K3sAgent.sh
+++ K3sAgent.sh
@@ -2,3 +2,4 @@
 export INSTALL_K3S_EXEC="agent"
 export K3S_CLUSTER_SECRET="<your random secret string that is the same on all nodes>"
 export K3S_URL="https://node0:6443"
+curl -sfL https://get.k3s.io | sh -
